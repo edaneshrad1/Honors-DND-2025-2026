b9aec4809acdabb0273d72717a70a72fcadb27bc
edaneshrad1 <101284380+edaneshrad1@users.noreply.github.com>
2025-08-27 21:14:55 -0700

    8/27 revisions

--- PugRescue/PugSaver.java
+++ PugRescue/PugSaver.java
@@ -8,14 +8,24 @@ public class PugSaver {
 		for (int i = 0; i < list.size(); i++) {
 			Dog firstDog = list.get(i);
 			String dogTypeFront = firstDog.getBreed();
-			int doesExistFront = dogTypeFront.indexOf("gold");
-			if (doesExistFront != -1) {
+			int isGoldFront = dogTypeFront.indexOf("gold");
+			if (isGoldFront != -1) {
 				for (int j = list.size() - 1; j >= 0; j--) {
 					Dog lastDog = list.get(j);
+					String dogTypeBack = lastDog.getBreed();
+					int isGoldBack = dogTypeBack.indexOf("gold");
+					int frontDogPos = i;
+					if (isGoldBack == -1) {
+						ArrayList<Dog> temp = new ArrayList<>();
+						temp.add(lastDog);
+						int lastDogPos = list.size() - j - 1;
+						list.set(lastDogPos, firstDog);
+						list.set(frontDogPos, temp.get(0));
+					}
 					//Find last dog in list that's not golden
 					//Put this dog in a temp array list
 					//Set the golden dog to the index of the last dog that's not golden
-					//Set the index 
+					//Set the temp to the origional index of the golden dog
 				}
 				// ArrayList temp = new ArrayList();
 				// Dog gold = list.get(i);
