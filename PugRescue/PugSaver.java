3c1f4f73e605b85fba086c99859d30c9d26d0ba8
edaneshrad1 <101284380+edaneshrad1@users.noreply.github.com>
2025-08-28 23:45:40 -0700

    8/28 revisions

--- PugRescue/PugSaver.java
+++ PugRescue/PugSaver.java
@@ -3,36 +3,26 @@ import java.util.Objects;
 
 public class PugSaver {
 
-	//Moves every dog whose breed is "Pug" in the list to the back of the list
+	// Moves every dog whose breed is "Pug" in the list to the back of the list
 	public static void rescuePugs(ArrayList<Dog> list) {
 		for (int i = 0; i < list.size(); i++) {
-			Dog firstDog = list.get(i);
-			String dogTypeFront = firstDog.getBreed();
-			int isGoldFront = dogTypeFront.indexOf("gold");
-			if (isGoldFront != -1) {
-				for (int j = list.size() - 1; j >= 0; j--) {
-					Dog lastDog = list.get(j);
-					String dogTypeBack = lastDog.getBreed();
-					int isGoldBack = dogTypeBack.indexOf("gold");
-					int frontDogPos = i;
-					if (isGoldBack == -1) {
-						ArrayList<Dog> temp = new ArrayList<>();
-						temp.add(lastDog);
-						int lastDogPos = list.size() - j - 1;
-						list.set(lastDogPos, firstDog);
-						list.set(frontDogPos, temp.get(0));
+			if (list.get(i).getBreed().indexOf("Gold") != -1) {
+				for (int j = list.size() - 1; j > 0; j--) {
+					if (list.get(j).getBreed().indexOf("Gold") == -1) {
+						if (i < j) {
+							Dog tempDog1 = list.get(i);
+							Dog tempDog2 = list.get(j);
+							list.set(i, tempDog2);
+							list.set(j, tempDog1);
+						}
 					}
-					//Find last dog in list that's not golden
-					//Put this dog in a temp array list
-					//Set the golden dog to the index of the last dog that's not golden
-					//Set the temp to the origional index of the golden dog
 				}
-				// ArrayList temp = new ArrayList();
-				// Dog gold = list.get(i);
-				// temp.add(list.get(i));
-				// list.set(list.size() - 1 - i, temp.get(i));
-				// list.set(li)
 			}
 		}
 	}
 }
+
+// Find last dog in list that's not golden
+// Put this dog in a temp array list
+// Set the golden dog to the index of the last dog that's not golden
+// Set the temp to the origional index of the golden dog
---
+++ PugRescue/pugTester.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+
+public class pugTester {
+    public static void main(String[] args) {
+        ArrayList<Dog> dogs = new ArrayList<Dog>();
+        Dog one = new Dog("fido", "Golden Retriever");
+        Dog two = new Dog("fluffy", "German Shepherd");
+        Dog three = new Dog("Mr. Theiss", "Golden lab");
+        Dog four = new Dog("Cookie", "Shitzu");
+        dogs.add(one);
+        dogs.add(two);
+        dogs.add(three);
+        dogs.add(four);
+        System.out.println("Before");
+        for (int i = 0; i < dogs.size(); i++) {
+            System.out.println(dogs.get(i).toString());
+        }
+        System.out.println();
+        PugSaver.rescuePugs(dogs);
+        System.out.println("After");
+        for (int i = 0; i < dogs.size(); i++) {
+            System.out.println(dogs.get(i).toString());
+        }
+    }
+}
