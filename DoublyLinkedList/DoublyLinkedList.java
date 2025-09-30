5d435899a421fe934c312b24cb7952ee68f63b52
edaneshrad1 <101284380+edaneshrad1@users.noreply.github.com>
2025-09-30 13:17:22 -0700

    9/30

--- DoublyLinkedList/DoublyLinkedList.java
+++ DoublyLinkedList/DoublyLinkedList.java
@@ -51,11 +51,29 @@ public class DoublyLinkedList {
 
 	// Returns true if this list contains an element equal to obj;
 	// otherwise returns false.
-	public boolean contains(Nucleotide obj) {}
+	public boolean contains(Nucleotide obj) {
+		for (ListNode2<Nucleotide> inspected =
+				SENTINEL.getNext(); inspected != SENTINEL; inspected = inspected.getNext()) {
+			if (inspected.getValue().equals(obj)) {
+				return true;
+			}
+		}
+		return false;
+	}
 
 	// Returns the index of the first element in equal to obj;
 	// if not found, returns -1.
-	public int indexOf(Nucleotide obj) {}
+	public int indexOf(Nucleotide obj) {
+		int index = 0;
+		for (ListNode2<Nucleotide> inspected =
+				SENTINEL.getNext(); inspected != SENTINEL; inspected = inspected.getNext()) {
+			if (inspected.getValue().equals(obj)) {
+				return index;
+			}
+			index++;
+		}
+		return -1;
+	}
 
 	// Adds obj to this collection. Returns true if successful;
 	// otherwise returns false.
@@ -78,27 +96,90 @@ public class DoublyLinkedList {
 
 	// Removes the first element that is equal to obj, if any.
 	// Returns true if successful; otherwise returns false.
-	public boolean remove(Nucleotide obj) {}
+	public boolean remove(Nucleotide obj) {
+		int index = 0;
+		for (ListNode2<Nucleotide> inspected =
+				SENTINEL.getNext(); inspected != SENTINEL; inspected = inspected.getNext()) {
+					if (inspected.getValue().equals(obj)) {
+						remove(index);
+						nodeCount--;
+						return true;
+					}
+					index++;
+		}
+		return false;
+	}
 
 	// Returns the i-th element.
-	public Nucleotide get(int i) {}
+	public Nucleotide get(int i) {
+		int index = 0;
+		ListNode2<Nucleotide> inspected = SENTINEL.getNext();
+		while (index < i) {
+			inspected = inspected.getNext();
+			index++;
+		}
+		return inspected.getValue();
+	}
 
 	// Replaces the i-th element with obj and returns the old value.
-	public Nucleotide set(int i, Nucleotide obj) {}
+	public Nucleotide set(int i, Nucleotide obj) {
+		int index = 0;
+		ListNode2<Nucleotide> inspected = SENTINEL.getNext();
+		while (index < i) {
+			inspected = inspected.getNext();
+			index++;
+		}
+		Nucleotide returnValue = inspected.getValue();
+		inspected.setValue(obj);
+		return returnValue;
+	}
 
 	// Inserts obj to become the i-th element. Increments the size
 	// of the list by one.
-	public void add(int i, Nucleotide obj) {}
+	public void add(int i, Nucleotide obj) {
+		int index = 0;
+		ListNode2<Nucleotide> inspected = SENTINEL.getNext();
+		ListNode2<Nucleotide> addedNode = new ListNode2<Nucleotide>(obj);
+		if (i == 0) {
+			SENTINEL.setNext(addedNode);
+			addedNode.setPrevious(SENTINEL);
+			addedNode.setNext(inspected);
+			inspected.setPrevious(addedNode);
+		}
+		while (index < i) {
+			inspected = inspected.getNext();
+			index++;
+		}
+		ListNode2<Nucleotide> nextNode = inspected.getNext();
+		inspected.setNext(addedNode);
+		addedNode.setPrevious(inspected);
+		addedNode.setNext(nextNode);
+		nextNode.setPrevious(addedNode);
+	}
 
 	// Removes the i-th element and returns its value.
 	// Decrements the size of the list by one.
-	public Nucleotide remove(int i) {}
+	public Nucleotide remove(int i) {
+		int index = 0;
+		ListNode2<Nucleotide> inspected = SENTINEL.getNext();
+		while (index < i) {
+			inspected = inspected.getNext();
+			index++;
+		}
+		ListNode2<Nucleotide> previousNode = inspected.getPrevious();
+		ListNode2<Nucleotide> nextNode = inspected.getNext();
+		previousNode.setNext(nextNode);
+		nextNode.setPrevious(previousNode);
+		nodeCount--;
+		return inspected.getValue();
+	}
 
 	// Returns a string representation of this list exactly like that for MyArrayList.
 	public String toString() {
 		StringBuilder returnString = new StringBuilder();
 		returnString.append('[');
-		for (ListNode2<Nucleotide> inspected = SENTINEL.getNext(); inspected != SENTINEL; inspected = inspected.getNext()) {
+		for (ListNode2<Nucleotide> inspected =
+				SENTINEL.getNext(); inspected != SENTINEL; inspected = inspected.getNext()) {
 			if (inspected != SENTINEL.getPrevious()) {
 				returnString.append(inspected.getValue());
 				returnString.append(", ");
