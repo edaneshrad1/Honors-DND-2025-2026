8ba9faa0ab0f0448d36b213938a8253275ef37f5
edaneshrad1 <101284380+edaneshrad1@users.noreply.github.com>
2025-10-22 23:08:56 -0700

    10/22

--- Recursion/RecursionTester.java
+++ Recursion/RecursionTester.java
@@ -4,21 +4,25 @@ public class RecursionTester {
         ListNode second = new ListNode("whaddup", third);
         ListNode head = new ListNode("head is here", second);
 
-        String[][] matrix = {
-        {"normal", "infected", "vaccinated", "infected"},
-        {"normal", "infected", "normal", "normal"}, 
-        {"vaccinated", "vaccinated", "normal", "infected"},
-        {"normal", "infected", "vaccinated", "normal"}
-    };
-        //Call my method with the 'head'
+        String[][] matrix = {{"normal", "infected", "vaccinated", "infected"},
+                {"normal", "infected", "normal", "normal"},
+                {"vaccinated", "vaccinated", "normal", "infected"},
+                {"normal", "infected", "vaccinated", "normal"}};
+                
+        // Call my method with the 'head'
         // Recursion.printListInReverse(head);
 
-        Recursion.infect(matrix, 0, 1);
-        for (int row = 0; row < matrix.length; row++) {
-            for (int col = 0; col < matrix[row].length; col++) {
-                System.out.print(matrix[row][col] + ", ");
-            }
-            System.out.println();
-        }
+        // Recursion.infect(matrix, 0, 1);
+        // for (int row = 0; row < matrix.length; row++) {
+        // for (int col = 0; col < matrix[row].length; col++) {
+        // System.out.print(matrix[row][col] + ", ");
+        // }
+        // System.out.println();
+        // }
+
+        // System.out.println(Recursion.countNonConsecutiveSubsets(22));
+
+        // System.out.println(Recursion.countWaysToJumpUpStairs(5));
+
     }
 }
